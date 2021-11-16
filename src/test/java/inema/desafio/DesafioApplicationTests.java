d9984c213e744d81c379e5294aae785dce0afee3
Roberto Bernardes <roberto.bernardes@inema.ba.gov.br>
2021-11-16 10:27:25 -0300

    Criando o projeto Spring Boot

---
+++ src/test/java/inema/desafio/DesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package inema.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
